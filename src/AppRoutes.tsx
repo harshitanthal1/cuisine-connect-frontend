40daa2edd00a9dedc1745ab2d25bf5bc4d04bf03
harshit <harshitanthal3@gmail.com>
2024-05-02 10:03:01 +0530

    manage restaurant

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 
 const AppRoutes = ()=>{
     return(
@@ -18,6 +19,14 @@ const AppRoutes = ()=>{
                     <UserProfilePage/>
                   </Layout>
                 } 
+              />
+              <Route
+                path="/manage-restaurant"
+                element={
+                  <Layout>
+                    <ManageRestaurantPage />
+                 </Layout>
+                }
               />
             </Route>
             
