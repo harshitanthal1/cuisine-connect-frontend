529d65bbb338eb2be9175e0242451fb6c1d6ff03
harshit <harshitanthal3@gmail.com>
2024-05-08 11:58:23 +0530

    added restaurant and order details

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,13 +1,19 @@
 import { CircleUserRound, Menu } from "lucide-react";
-import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
 import MobileNavLinks from "./MobileNavLinks";
 
-
 const MobileNav = () => {
   const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -21,7 +27,7 @@ const MobileNav = () => {
               {user?.email}
             </span>
           ) : (
-            <span> Welcome to Cuisine Connect! </span>
+            <span> Welcome to MernEats.com!</span>
           )}
         </SheetTitle>
         <Separator />
@@ -29,18 +35,17 @@ const MobileNav = () => {
           {isAuthenticated ? (
             <MobileNavLinks />
           ) : (
-            <Button 
-              onClick={()=> loginWithRedirect()} 
-              className="flex-1 font-bold bg-orange-500 py-2 px-4 text-lg md:py-3 md:px-6 md:text-xl"
-            > 
+            <Button
+              onClick={() => loginWithRedirect()}
+              className="flex-1 font-bold bg-orange-500"
+            >
               Log In
-            </Button> 
+            </Button>
           )}
-        
         </SheetDescription>
       </SheetContent>
     </Sheet>
   );
-}
+};
 
 export default MobileNav;
